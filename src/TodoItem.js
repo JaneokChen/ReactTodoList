09f15a09fc245509575dabe5d67ae9abcd854e29
Wanttofeedacat <247345858@qq.com>
2021-04-26 10:51:01 +0800

    简化了代码

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,25 +1,22 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react';
 
-class TodoItem extends Component {
+class TodoItem extends Component{
   constructor(props){
     super(props)
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
 
   render(){
-    return (
-      <div
-        key={this.props.index}
-        onClick={this.handleClick}
-      >
+    return(
+      <div onClick={this.handleClick}>
         {this.props.content}
-      </div>
-    );
+      </div>)
   }
 
   handleClick(){
     this.props.deleteItem(this.props.index)
   }
+
 }
 
 export default TodoItem
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,62 +1,71 @@
-import React, {Component, Fragment} from 'react'
+import React, {Component,Fragment} from 'react'
 import TodoItem from './TodoItem'
-import './style.css'
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
+class TodoList extends Component{
+  constructor(props){
+    super(props)
     this.state = {
       inputValue: '',
-      list: [],
-    };
+      list: []
+    }
+
+    this.handleItem = this.handleItem.bind(this)
+    this.handleItemClick = this.handleItemClick.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this);
   }
 
-  render() {
-    return (
+  render(){
+    return(
       <Fragment>
         <label htmlFor="inputArea">输入内容</label>
         <input
           id="inputArea"
-          value = {this.state.inputValue}
-          onChange={this.handleInputChange.bind(this)} />
-        <button onClick={this.handleBtnClick.bind(this)}>提交</button>
-        <ul>
-          {this.state.list.map((item, index) => {
-            return (
-              <TodoItem
-                content={item}
-                index={index}
-                deleteItem={this.handleItemClick.bind(this)}
-              />
-            )
-          })}
-        </ul>
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleBtnClick}>提交</button>
+        <ul>{this.handleItem()}</ul>
       </Fragment>
-    );
+    )
   }
 
-  handleInputChange(e) {
-    const value = e.target.value
-    this.setState({
-      inputValue:value
+  handleItem(){
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={item}
+          index={index}
+          deleteItem={this.handleItemClick}
+        />
+      )
     })
   }
 
-  handleBtnClick() {
-    let list = [...this.state.list, this.state.inputValue];
+  handleInputChange(e){
+    const value = e.target.value
+    this.setState({
+      inputValue: value
+    })
+  }
+
+  handleBtnClick(){
+    const list = [...this.state.list, this.state.inputValue]
     this.setState({
       list,
       inputValue: ''
     })
   }
 
-  handleItemClick(index) {
-    let list = this.state.list
-    list.splice(index, 1)
+  handleItemClick(index){
+    const list = [...this.state.list];
+    list.splice(index, 1);
     this.setState({
       list
-    })
+    });
   }
+
 }
 
 export default TodoList
